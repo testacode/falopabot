2f5cb5b47f9daa46f21d9209537039c9e2ee2e01
Carlos Monti <carlosmonti@outlook.com>
2020-05-30 21:39:35 -0300

    Agregando ejemplo de preguntas

--- src/index.js
+++ src/index.js
@@ -6,7 +6,45 @@ const truncate = require('lodash/truncate');
 const chistesDB = require('./db');
 
 const TeleBot = require('telebot');
-const bot = new TeleBot(process.env.BOT_TOKEN);
+const bot = new TeleBot({
+  token: process.env.BOT_TOKEN,
+  usePlugins: ['askUser']
+});
+
+bot.on('/start', msg => {
+  const { from } = msg;
+  const id = from.id;
+
+  // Preguntar nombre
+  return bot.sendMessage(id, 'Hola, cuál es tu nombre?', { ask: 'nombre' });
+});
+
+// Evento preguntar nombre
+bot.on('ask.nombre', msg => {
+  const { from, text: nombre } = msg;
+  const id = from.id;
+
+  // Preguntar edad
+  return bot.sendMessage(id, `Mucho gusto, ${nombre}! Cuántos años tenes?`, { ask: 'edad' });
+});
+
+// Evento preguntar edad
+bot.on('ask.edad', msg => {
+  const { from, text } = msg;
+  const id = from.id;
+  const edad = Number(text);
+
+  if (!edad) {
+
+    // Si la edad es incorrecta, preguntar de nuevo
+    return bot.sendMessage(id, 'Esa edad es falopa, dale! Cuántos años tenes?', { ask: 'edad' });
+
+  } else {
+
+    // Último mensaje
+    return bot.sendMessage(id, `Tenes ${edad} años. A la flauta!`);
+  }
+});
 
 // "edit message" action
 bot.on('edit', (msg) => {
